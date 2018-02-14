a1bf53adc78020e847289bd15d046d48167b4e7a
EC2 Default User <ec2-user@ip-172-31-27-103.us-east-2.compute.internal>
2018-02-14 00:45:36 +0000

    HW1 code

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,55 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum_1 = 0
+  arr.each do |i|
+    sum_1 += i
+  end
+  return sum_1
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr.first if arr.length == 1
+  arr.sort{ |x, y| y <=> x }.take(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty?
+  (arr.empty? && n.zero?) || arr.permutation(2).any? { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return /^[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s == "0"
+	return /^[10]*00$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+	attr_accessor :price
+	
+  def initialize(isbn, price)
+    if isbn.empty? or price <=0
+       raise ArgumentError.new("Argument not Allowed")
+    end
+     @isbn = isbn
+     @price = price
+  end
+  
+  def price_as_string
+    sprintf("$%2.2f", @price)
+  end
+
 end
